a7acc790c9f45835a6d0f89f5c251e67f9493597
FerDiaz19 <158664095+FerDiaz19@users.noreply.github.com>
2025-05-30 20:23:00 -0700

    basics pages

--- public/views.py
+++ public/views.py
@@ -8,9 +8,16 @@ from django.views import generic
 
 class HomeView(generic.View):
     template_name = 'public/index.html'
-    context = { 'message': 'AXYOMA PROJECT :)' }
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, self.context)
+        section = request.GET.get('section', 'inicio')
+        if section == 'seccion1':
+            content = "Aquí va algo de la Sección 1."
+        elif section == 'seccion2':
+            content = "Aquí va algo de la Sección 2."
+        else:
+            content = "Aquí va la landing general."
+        context = { 'section': section, 'content': content }
+        return render(request, self.template_name, context)
 
 # ---------------------------------------------------------------------------- #
