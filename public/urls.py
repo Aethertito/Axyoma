ef03bca28323919903f9316ff6e49a67bb54942e
FerDiaz19 <158664095+FerDiaz19@users.noreply.github.com>
2025-06-15 21:16:49 -0700

    Dashboard

--- public/urls.py
+++ public/urls.py
@@ -4,11 +4,14 @@
 from django.urls import path
 from . import views
 
+# ---------------------------------------------------------------------------- #
+
 
 app_name = 'public'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='HomeView'),
+    path('dashboard/', views.dashboard_home, name='dashboard_home'),
 ]
 
 # ---------------------------------------------------------------------------- #
--- public/views.py
+++ public/views.py
@@ -12,5 +12,17 @@ class HomeView(generic.View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.context)
+    
+def dashboard_home(request):
+    users = [
+        {'name': 'Juan', 'age': 30, 'country': 'México'},
+        {'name': 'Ana', 'age': 25, 'country': 'Colombia'},
+        {'name': 'Luis', 'age': 28, 'country': 'Perú'},
+    ]
+    chart_data = [12, 5, 8]
+    return render(request, 'dashboard/home.html', {
+    'users': users,
+    'chart_data': chart_data,
+})
 
 # ---------------------------------------------------------------------------- #
